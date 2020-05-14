7e60dc2c31410648cdafafaf5273b80290a35d38
Irene Scott <irenejinscott@gmail.com>
2020-05-14 22:12:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,18 +28,16 @@ class Song
      @@genres.uniq!
   end
   
-  def self.genre_count
-   genre_count = Hash.new(0)
-   @@genres.each do |genre|
-      genre_count[genre]
-      @@genres += 1
-    end
-   end
-   
    def self.genre_count
     genre_count = Hash.new(0)
     @@genres.each {|genre| genre_count[genre] += 1}
     genre_count
+  end
+  
+  def self.artists_count
+    artists_count = Hash.new(0)
+    @@artists.each {|artist| artists_count[artist] += 1}
+    artists_count
   end
   
   
